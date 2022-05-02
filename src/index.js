8fc251ea4f2bf7e908c28176ef025844c4c311b3
Anderson F. Vasconcelos <anderson.fv.rj@gmail.com>
2022-05-02 17:47:55 -0300

    .

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const app = express()
-const port = 3000
 
 app.get('/', (res) => {
     res.send("<h1> A soma é: " + soma(5,2) + "</h1>");
@@ -11,10 +10,6 @@ function soma(x, y) {
     return x + y;
 
 }
-
-app.listen(port, ()=> {
-    console.log('Rodando na porta ' + port)
-})
 
 exports.soma = soma;
 
